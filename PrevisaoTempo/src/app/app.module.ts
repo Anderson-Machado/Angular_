aa1d06a032fca56dde7ba87e019d18598cb1c4f8
Anderson-Machado <machado.analista@gmail.com>
2018-11-02 22:51:38 -0500

    Gerando a pagina para trazer a previsão do tempo

--- PrevisaoTempo/src/app/app.module.ts
+++ PrevisaoTempo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TempoComponent } from './tempo/tempo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempoComponent
   ],
   imports: [
     BrowserModule,
